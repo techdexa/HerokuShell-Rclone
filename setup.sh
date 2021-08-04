f21ea677b3e48f4b2f9579d553a26418dd51f239
techdexa <86263363+techdexa@users.noreply.github.com>
2021-08-04 15:31:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+apt-get -qq update && apt-get install -y neofetch
 # Install rclone static binary
 wget -q https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip -qqj rclone-current-linux-amd64.zip
